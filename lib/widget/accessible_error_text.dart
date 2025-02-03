f0752eaa11635b13947b617e178b6345e8c077cd
Thillainthan Gowthaman <gowthamangtm@gmail.com>
2025-02-03 17:21:25 +0530

    bug fix

--- lib/widget/accessible_error_text.dart
+++ lib/widget/accessible_error_text.dart
@@ -112,12 +112,3 @@ class AccessiblyErrorText extends StatelessWidget {
     );
   }
 }
-
-Size _textSize(String text, TextStyle style) {
-  final TextPainter textPainter = TextPainter(
-      text: TextSpan(text: text, style: style),
-      maxLines: 1,
-      textDirection: TextDirection.ltr)
-    ..layout(minWidth: 0, maxWidth: double.infinity);
-  return textPainter.size;
-}
